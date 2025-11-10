fa6091876ada1a436eb0359b0f40dfe039828512
Moritz <moritz@luxner-zt.com>
2025-11-10 11:28:48 +0100

    Init Commit

---
+++ src/main/java/com/stocksim/stocksim/StockSimApplication.java
@@ -0,0 +1,13 @@
+package com.stocksim.stocksim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockSimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stocksim/stocksim/StockSimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocksim.stocksim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
