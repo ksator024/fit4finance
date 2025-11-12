85035772cbb4fc00e1537fac6e5bee7aeea1bb3b
Fabio <fabio08of@outlook.com>
2025-11-12 14:17:44 +0100

    -Wichtige änderung in der Main!!

--- src/main/java/com/stocksim/stocksim/StockSimApplication.java
+++ src/main/java/com/stocksim/stocksim/StockSimApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StockSimApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(StockSimApplication.class, args);
+        System.out.println("Das gitignore ist vermutlich scheiße.");
     }
 
 }
