a598d1658086e6b63b6b4a29c9ab060fa3f94775
Moritz <moritz@luxner-zt.com>
2026-01-26 12:14:05 +0100

    stock names update

--- src/main/java/com/stocksim/stocksim/ScenarioManager.java
+++ src/main/java/com/stocksim/stocksim/ScenarioManager.java
@@ -8,9 +8,9 @@ public  class ScenarioManager {
 
    static Scenario getScenario(int number){
 
-   scenarios.add(new Scenario(915200940, 1034170140+86400 , new ArrayList<>(Arrays.asList("CSCO", "INTC","AMZN","MSFT")), 100000.0));
-   scenarios.add(new Scenario(1577888940, 1640960940+86400 , new ArrayList<>(Arrays.asList("DIS", "XOM","JPM","BAC","AAPL","MSFT","NVDA")), 3));
-   scenarios.add(new Scenario(1167661740, 1236608940+86400 , new ArrayList<>(Arrays.asList("BAC", "JPM","AAPL","MSFT","JNJ","PG","UNH")), 2));
+   scenarios.add(new Scenario(915200940, 1034170140+86400 , new ArrayList<>(Arrays.asList("CSCO", "INTC","AMZN")), 100000.0));
+   scenarios.add(new Scenario(1577888940, 1640960940+86400 , new ArrayList<>(Arrays.asList("DIS", "XOM","NVDA")), 3));
+   scenarios.add(new Scenario(1167661740, 1236608940+86400 , new ArrayList<>(Arrays.asList("BAC", "JPM","AAPL")), 2));
    System.out.println("SCNEARIO NUMBER: " + number);
    return scenarios.get(number-1);
    }
